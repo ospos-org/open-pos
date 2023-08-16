3cb4a2ff826f6e43640f3382dd58ff227d6ba554
Ben White <benwhite2413@gmail.com>
2023-08-16 11:28:31 +1000

    mod: env change

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,15 @@
 
 echo "Check that we have NEXT_PUBLIC_API_URL vars"
 test -n "$NEXT_PUBLIC_API_URL"
+echo $NEXT_PUBLIC_API_URL
+
+echo "Check that we have NEXT_PUBLIC_SELF_URL vars"
+test -n "$NEXT_PUBLIC_SELF_URL"
+echo $NEXT_PUBLIC_SELF_URL
 
 echo "Check that we have NEXT_PUBLIC_DEMO vars"
 test -n "$NEXT_PUBLIC_DEMO"
+echo $NEXT_PUBLIC_DEMO
 
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_API_URL#$NEXT_PUBLIC_API_URL#g"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_DEMO#$NEXT_PUBLIC_DEMO#g"
