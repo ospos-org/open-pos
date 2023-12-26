ca9757717ae734a30d6b2e7782d43fcb41c8dce4
Ben White <contact@bennjii.dev>
2023-12-27 11:24:50 +1300

    mod: refactor productTitle

--- src/components/kiosk/children/order/individual/orderTitle.tsx
+++ src/components/kiosk/children/order/individual/orderTitle.tsx
@@ -3,6 +3,9 @@ import Image from "next/image"
 
 import { ordersAtom } from "@atoms/transaction"
 import {ContextualOrder} from "@utils/stockTypes";
+import OrderTypeIcon from "@components/kiosk/children/order/individual/orderTypeIcon";
+import OrderHeader from "@components/kiosk/children/order/individual/orderHeader";
+import OrderDestination from "@components/kiosk/children/order/individual/orderDestination";
 
 interface OrderTitleProps {
     currentOrder: ContextualOrder,
@@ -12,90 +15,15 @@ interface OrderTitleProps {
 export function OrderTitle({ currentOrder, index }: OrderTitleProps) {
     const orderState = useAtomValue(ordersAtom)
 
-    if (orderState.length !== 1) {
+    if (orderState.length !== 1 || orderState[0].order_type !== "direct") {
         return (
             <div className={`flex select-none flex-row w-full justify-between gap-2 ${index == 0 ? "" : "mt-4"}`}>
                 <div className="flex flex-col gap-1">
-                    <div className="flex flex-row items-center gap-2 select-none">
-                        {
-                            currentOrder.order_type == "pickup" ?
-                            <Image src="/icons/building-02.svg" alt="" height={20} width={20} style={{ filter: "invert(100%) sepia(100%) saturate(0%) hue-rotate(299deg) brightness(102%) contrast(102%)" }} />
-                            :
-                            currentOrder.order_type == "quote" ?
-                            <Image src="/icons/globe-05.svg" alt="" height={20} width={20} style={{ filter: "invert(100%) sepia(100%) saturate(0%) hue-rotate(299deg) brightness(102%) contrast(102%)" }} />
-                            :
-                            currentOrder.order_type == "shipment" ?
-                            <Image src="/icons/globe-05.svg" alt="" height={20} width={20} style={{ filter: "invert(100%) sepia(100%) saturate(0%) hue-rotate(299deg) brightness(102%) contrast(102%)" }} />
-                            :
-                            <Image src="/icons/shopping-bag-01-filled.svg" alt="" height={20} width={20} style={{ filter: "invert(100%) sepia(100%) saturate(0%) hue-rotate(299deg) brightness(102%) contrast(102%)" }} />
-                        }
-
-                        <div className="text-white font-semibold flex flex-row items-center gap-2">
-                            { currentOrder.order_type == "pickup" ? currentOrder.destination?.contact?.name : currentOrder.order_type == "direct" ? "Instore Purchase" : currentOrder?.origin?.contact?.name} 
-
-                            {
-                                currentOrder.order_type !== "pickup" && currentOrder.order_type !== "direct" && currentOrder.order_type !== "quote" ?
-                                <p className="text-gray-400"> -&gt; {currentOrder.destination?.contact.address.street}</p>
-                                :
-                                <></>
-                            }
-                        </div>
-                    </div>
-                    
-                    { 
-                        currentOrder.order_type == "pickup" ? 
-                        <p className="text-gray-400">{currentOrder.destination?.contact.address.street}, {currentOrder.destination?.contact.address.street2}, {currentOrder.destination?.contact.address.po_code}</p>
-                        :
-                        currentOrder.order_type !== "direct" && currentOrder.order_type !== "quote" ?
-                        <p className="text-gray-400">{currentOrder.origin?.contact.address.street}, {currentOrder.origin?.contact.address.street2}, {currentOrder.origin?.contact.address.po_code}</p>
-                        :
-                        <></>
-                    }
+                    <OrderHeader currentOrder={currentOrder} />
+                    <OrderDestination currentOrder={currentOrder} />
                 </div>
             </div>
         )
-    }
-
-    if (orderState[0].order_type !== "direct") {
-       return (
-            <div className={`flex select-none flex-row w-full justify-between gap-2 ${index == 0 ? "" : "mt-4"}`}>
-                <div className="flex flex-col gap-1">
-                    <div className="flex flex-row items-center gap-2 select-none">
-                        {
-                            currentOrder.order_type == "pickup" ?
-                            <Image src="/icons/building-02.svg" alt="" height={20} width={20} style={{ filter: "invert(100%) sepia(100%) saturate(0%) hue-rotate(299deg) brightness(102%) contrast(102%)" }} />
-                            :
-                            currentOrder.order_type == "quote" ?
-                            <Image src="/icons/globe-05.svg" alt="" height={20} width={20} style={{ filter: "invert(100%) sepia(100%) saturate(0%) hue-rotate(299deg) brightness(102%) contrast(102%)" }} />
-                            :
-                            currentOrder.order_type == "shipment" ?
-                            <Image src="/icons/globe-05.svg" alt="" height={20} width={20} style={{ filter: "invert(100%) sepia(100%) saturate(0%) hue-rotate(299deg) brightness(102%) contrast(102%)" }} />
-                            :
-                            <Image src="/icons/globe-05.svg" alt="" height={20} width={20} style={{ filter: "invert(100%) sepia(100%) saturate(0%) hue-rotate(299deg) brightness(102%) contrast(102%)" }} />
-                        }
-
-                        <div className="text-white font-semibold flex flex-row items-center gap-2">
-                            { currentOrder.order_type == "pickup" ? currentOrder.destination?.contact?.name : currentOrder.origin?.contact?.name} 
-                            {/* <p className="text-gray-400">({ currentOrder.order_type == "pickup" ? currentOrder.destination?.store_code : currentOrder.origin?.store_code})</p>  */}
-                            
-                            {
-                                currentOrder.order_type !== "pickup" ?
-                                <p className="text-gray-400"> -&gt; {currentOrder.destination?.contact.address.street}</p>
-                                :
-                                <></>
-                            }
-                        </div>
-                    </div>
-                    
-                    { 
-                        currentOrder.order_type == "pickup" ? 
-                        <p className="text-gray-400">{currentOrder.destination?.contact.address.street}, {currentOrder.destination?.contact.address.street2}, {currentOrder.destination?.contact.address.po_code}</p>
-                        :
-                        <p className="text-gray-400">{currentOrder.origin?.contact.address.street}, {currentOrder.origin?.contact.address.street2}, {currentOrder.origin?.contact.address.po_code}</p>
-                    } 
-                </div>
-            </div>
-       )
     }
 
     return <></>
--- src/components/kiosk/children/order/individual/productTitle.tsx
+++ src/components/kiosk/children/order/individual/productTitle.tsx
@@ -3,6 +3,7 @@ import { useSetAtom } from "jotai"
 import { inspectingProductAtom } from "@atoms/product"
 import {ContextualOrder, ContextualProductPurchase} from "@utils/stockTypes"
 import {Stock} from "@/generated/stock/Api";
+import {useCallback, useMemo} from "react";
 
 interface ProductTitleProps {
     currentOrder: ContextualOrder,
@@ -14,31 +15,43 @@ interface ProductTitleProps {
 export function ProductTitle({ currentOrder, product, quantityHere, totalStock }: ProductTitleProps) {
     const setInspectingProduct = useSetAtom(inspectingProductAtom)
 
+    const focusProduct = useCallback(() => {
+        setInspectingProduct((currentProduct) => ({
+            ...currentProduct,
+            activeProduct: product.product,
+            activeProductVariant: product.variant_information,
+            activeProductPromotions: product.active_promotions
+        }))
+    }, [product, setInspectingProduct])
+
+    const outOfStock = useMemo(() => {
+        const quantityConsumed = (
+            currentOrder.products.reduce((t, i) =>
+                i.variant_information.barcode == product.variant_information.barcode ? i.quantity : 0,
+            0) ?? 1
+        )
+
+        return quantityConsumed > (quantityHere?.quantity.quantity_sellable ?? 0)
+    }, [currentOrder.products, product, quantityHere?.quantity.quantity_sellable])
+
     return (
-        <div className="flex-1 cursor-pointer"
-            onClick={() => {
-                setInspectingProduct((currentProduct) => ({
-                    ...currentProduct,
-                    activeProduct: product.product,
-                    activeProductVariant: product.variant_information,
-                    activeProductPromotions: product.active_promotions
-                }))
-            }} >
+        <div
+            className="flex-1 cursor-pointer"
+            onClick={focusProduct}
+        >
             <p className="font-semibold">{product.product.company} {product.product.name}</p>
             <p className="text-sm text-gray-400">{product.variant_information.name}</p>
             
-            {
-                currentOrder.order_type == "direct" && product.transaction_type === "Out" ?
-                    (currentOrder.products.reduce((t, i) => t += (i.variant_information.barcode == product.variant_information.barcode ? i.quantity : 0), 0) ?? 1) 
-                    > 
-                    (quantityHere?.quantity.quantity_sellable ?? 0)
-                    ?
-                        <p className="text-xs text-red-400">Out of stock - {(quantityHere?.quantity.quantity_sellable ?? 0)} here, {(totalStock - (quantityHere?.quantity.quantity_sellable ?? 0)) ?? 0} in other stores</p>
-                    :
-                        <></>
-                :
-                    <></>
-            }
+            {Boolean(
+                currentOrder.order_type == "direct" && product.transaction_type === "Out"
+                &&
+                outOfStock
+            ) && (
+                <p className="text-xs text-red-400">
+                    Out of stock - {(quantityHere?.quantity.quantity_sellable ?? 0)} here
+                    , {(totalStock - (quantityHere?.quantity.quantity_sellable ?? 0)) ?? 0} in other stores
+                </p>
+            )}
         </div>
     )
 }
