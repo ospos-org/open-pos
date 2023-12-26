506124477ebbbec4f7a64c96e3320206439c258c
Ben White <contact@bennjii.dev>
2023-12-27 10:43:08 +1300

    mod: refactor cartActionHeader and Footer

--- src/components/kiosk/children/order/cartActionFooter.tsx
+++ src/components/kiosk/children/order/cartActionFooter.tsx
@@ -1,32 +1,46 @@
 import { useAtomValue, useSetAtom } from "jotai";
+import {useCallback} from "react";
 
 import { priceAtom, probingPricePayableAtom } from "@atoms/payment";
 import { kioskPanelLogAtom, parkSaleAtom } from "@atoms/kiosk";
-import { ordersAtom } from "@atoms/transaction";
+import {totalProductQuantityAtom} from "@atoms/cart";
 
 export function CartActionFooter() {
     const orderInfo = useAtomValue(priceAtom)
-    const orderState = useAtomValue(ordersAtom)
+    const orderProductQuantity = useAtomValue(totalProductQuantityAtom)
 
     const parkSale = useSetAtom(parkSaleAtom)
     const setKioskPanel = useSetAtom(kioskPanelLogAtom)
     const setProbePrice = useSetAtom(probingPricePayableAtom)
 
+    const moveToCheckout = useCallback(() => {
+        setKioskPanel("select-payment-method");
+        setProbePrice(orderInfo?.total);
+    }, [orderInfo?.total, setKioskPanel, setProbePrice])
+
     return (
         <div className="flex flex-row items-center gap-4">
             <div 
                 onClick={parkSale}
-                className={`bg-gray-300 w-full rounded-md p-4 flex items-center justify-center cursor-pointer ${(orderState.reduce((p, c) => p + c.products.reduce((prev, curr) => { return prev + curr.quantity }, 0), 0)) ?? 0 > 0 ? "" : "bg-opacity-10 opacity-20"}`}>
+                className={`
+                    bg-gray-300 w-full rounded-md p-4 flex items-center justify-center cursor-pointer 
+                    ${Boolean(orderProductQuantity <= 0) && "bg-opacity-10 opacity-20"}
+                `}
+            >
                 <p className="text-gray-800 font-semibold">Park Sale</p>
             </div>
 
             <div
-                onClick={() => {
-                    setKioskPanel("select-payment-method");
-                    setProbePrice(orderInfo?.total);
-                }} 
-                className={`${(orderState.reduce((p, c) => p + c.products.reduce((prev, curr) => { return prev + curr.quantity }, 0), 0) ?? 0) > 0 ? "bg-blue-700 cursor-pointer" : "bg-blue-700 bg-opacity-10 opacity-20"} w-full rounded-md p-4 flex items-center justify-center`}>
-                <p className={`text-white font-semibold ${""}`}>Checkout</p>
+                onClick={moveToCheckout}
+                className={
+                    (
+                        orderProductQuantity > 0
+                        ? "bg-blue-700 cursor-pointer"
+                        : "bg-blue-700 bg-opacity-10 opacity-20"
+                    ) + "w-full rounded-md p-4 flex items-center justify-center"
+                }
+            >
+                <p className="text-white font-semibold">Checkout</p>
             </div>
         </div>
     )
--- src/components/kiosk/children/order/cartActionHeader.tsx
+++ src/components/kiosk/children/order/cartActionHeader.tsx
@@ -1,15 +1,22 @@
 import { useAtom, useAtomValue, useSetAtom } from "jotai"
-
-import { customerAtom } from "@atoms/customer"
-import Image from "next/image"
-import { searchTypeHandlerAtom } from "@/src/atoms/search"
 import { useResetAtom } from "jotai/utils"
-import { defaultKioskAtom, perfAtom } from "@/src/atoms/kiosk"
-import { ordersAtom } from "@/src/atoms/transaction"
+import Image from "next/image"
+
+import { defaultKioskAtom, perfAtom } from "@atoms/kiosk"
+import { totalProductQuantityAtom } from "@atoms/cart"
+import { searchTypeHandlerAtom } from "@atoms/search"
+import { customerAtom } from "@atoms/customer"
+
+const FILTER_GRAY =
+    "invert(59%) sepia(9%) saturate(495%) hue-rotate(175deg) brightness(93%) contrast(95%)"
+const FILTER_WHITE =
+    "invert(100%) sepia(0%) saturate(7484%) hue-rotate(116deg) brightness(96%) contrast(101%)"
+const FILTER_ARROW =
+    "invert(100%) sepia(5%) saturate(7417%) hue-rotate(235deg) brightness(118%) contrast(101%)"
 
 export function CartActionHeader() {
     const perfState = useAtomValue(perfAtom)
-    const orderState = useAtomValue(ordersAtom)
+    const productQuantity = useAtomValue(totalProductQuantityAtom)
 
     const setSearchType = useSetAtom(searchTypeHandlerAtom)
     const resetCart = useResetAtom(defaultKioskAtom)
@@ -26,48 +33,60 @@ export function CartActionHeader() {
 
                         <Image
                             onClick={() => setCustomerState(null)} 
-                            className="cursor-pointer" height={15} width={15} src="/icons/x-2.svg" alt="" style={{ filter: "invert(59%) sepia(9%) saturate(495%) hue-rotate(175deg) brightness(93%) contrast(95%)" }}></Image>
+                            className="cursor-pointer"
+                            height={15} width={15}
+                            src="/icons/x-2.svg" alt=""
+                            style={{ filter: FILTER_GRAY }}
+                        />
                     </div>
                     :
                     <div 
                         onClick={() => setSearchType("customers")}
-                        className="bg-gray-800 rounded-md px-2 py-[0.125rem] flex flex-row items-center gap-2 cursor-pointer"
+                        className={
+                            "bg-gray-800 rounded-md px-2 py-[0.125rem] " +
+                            "flex flex-row items-center gap-2 cursor-pointer"
+                        }
                     >
                         <p>Select Customer</p>
 
                         <Image 
-                            className=""
-                            height={15} width={15} src="/icons/arrow-narrow-right.svg" alt="" style={{ filter: "invert(100%) sepia(5%) saturate(7417%) hue-rotate(235deg) brightness(118%) contrast(101%)" }}></Image>
+                            height={15} width={15}
+                            src="/icons/arrow-narrow-right.svg"
+                            alt="" style={{ filter: FILTER_ARROW }}
+                        />
                     </div>
                 }
+
                 <div className="text-sm text-gray-400">
                     {
-                        orderState.reduce((p, c) => p + c.products.reduce((prev, curr) => { return prev + curr.quantity }, 0), 0) == 0
-                        ? 
-                        "Cart Empty" 
-                        : 
-                        <p>
-                            {orderState.reduce((p, c) => p + c.products.reduce((prev, curr) => { return prev + curr.quantity }, 0), 0)} item{((orderState.reduce((p, c) => p + c.products.reduce((prev, curr) => { return prev + curr.quantity }, 0), 0) ?? 0) > 1 ? "s" : "")}
-                        </p>
+                        productQuantity
+                        ? "Cart Empty"
+                        : <p>{productQuantity} item{productQuantity > 1 ? "s" : ""}</p>
                     }
                 </div>
             </div>
-            
-            <div className="flex flex-row items-center gap-4">
-                { perfState.type === "continuative" ? 
-                        <div className="flex flex-row items-center gap-[0.75rem] bg-blue-800 py-1 px-3 rounded-md">
-                            <Image className="select-none svg cursor-pointer" width={15} height={15} src="/icons/coins-hand.svg" alt="" style={{ filter: "invert(100%) sepia(0%) saturate(7484%) hue-rotate(116deg) brightness(96%) contrast(101%)" }}></Image>
-                            <p className="text-white uppercase select-none font-bold text-sm">Refund</p>
-                        </div> 
-                    : 
-                        <></> 
-                }
-                
 
-                <div className="flex flex-row items-center gap-[0.75rem] bg-gray-800 p-2 px-4 rounded-md cursor-pointer">
-                    <p className="text-white select-none" onClick={() => {
-                        resetCart()
-                    }}>Clear Cart</p>
+            <div className="flex flex-row items-center gap-4">
+                {Boolean(perfState.type === "continuative") &&
+                    <div className="flex flex-row items-center gap-[0.75rem] bg-blue-800 py-1 px-3 rounded-md">
+                        <Image
+                            className="select-none svg cursor-pointer"
+                            width={15} height={15} src="/icons/coins-hand.svg"
+                            alt=""
+                            style={{filter: FILTER_WHITE }}
+                        />
+
+                        <p className="text-white uppercase select-none font-bold text-sm">Refund</p>
+                    </div>
+                }
+
+                <div
+                    className={
+                        "flex flex-row items-center gap-[0.75rem] " +
+                        "bg-gray-800 p-2 px-4 rounded-md cursor-pointer"
+                    }
+                >
+                    <p className="text-white select-none" onClick={() => resetCart()}>Clear Cart</p>
                 </div>
             </div>
         </div> 
