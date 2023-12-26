9af47a5a46a1ba172bf0173558fa87a975dd853a
Ben White <contact@bennjii.dev>
2023-12-27 10:27:22 +1300

    mod: migrated relatedMenu

--- src/components/kiosk/children/order/relatedMenu.tsx
+++ src/components/kiosk/children/order/relatedMenu.tsx
@@ -1,13 +1,37 @@
 import { useAtomValue, useSetAtom } from "jotai";
-import { useEffect, useState } from "react";
+import {useEffect, useMemo, useState} from "react";
 import Image from "next/image";
 import moment from "moment";
 
 import { inspectingTransactionAtom } from "@atoms/transaction";
 import { inspectingProductAtom } from "@atoms/product";
 import { kioskPanelLogAtom } from "@atoms/kiosk";
-import {Transaction} from "@/generated/stock/Api";
+import {Order, Transaction} from "@/generated/stock/Api";
 import {openStockClient} from "~/query/client";
+
+function orderType(order: Order, transaction: Transaction) {
+    switch(order.order_type) {
+        case "direct":
+            return `${order.origin.contact.name} (${order.origin.store_code})`
+        case "pickup":
+            return `${order.origin.contact.name} (${order.origin.store_code})`
+        case "quote":
+            return `By ${transaction.salesperson} at ${order.origin.contact.name} (${order.origin.store_code})`
+        case "shipment":
+            const origin = `${order.origin.contact.name} (${order.origin.store_code})`
+            const destination = `${order.destination?.store_code !== "000" 
+                ? order.destination?.store_code 
+                : order.destination?.contact.address.street
+            } ${order.destination?.store_code !== "000" 
+                ? order.destination?.contact.name 
+                : order.destination?.contact.address.street2
+            }`
+
+            return `${origin} -> ${destination}`
+        default:
+            return ""
+    }
+}
 
 export function RelatedOrders() {
     const setInspectingTransaction = useSetAtom(inspectingTransactionAtom)
@@ -25,8 +49,21 @@ export function RelatedOrders() {
                 .then(data => data.ok && setSuggestions(data.data))
     }, [inspectingProduct.activeProductVariant]);
 
+    const sortedSuggestions = useMemo(() =>
+        suggestions?.sort((a,b) =>
+            new Date(a.order_date).getTime() - new Date(b.order_date).getTime()
+        ),
+        [suggestions]
+    )
+
     return (
-        <div className="bg-gray-900 max-h-[calc(100vh - 18px)] overflow-auto p-6 flex flex-col h-full justify-between flex-1 gap-8" style={{ maxWidth: "min(550px, 100vw)", minWidth: "min(100vw, 550px)" }}>
+        <div
+            className={
+                "bg-gray-900 max-h-[calc(100vh - 18px)] overflow-auto " +
+                "p-6 flex flex-col h-full justify-between flex-1 gap-8"
+            }
+            style={{ maxWidth: "min(550px, 100vw)", minWidth: "min(100vw, 550px)" }}
+        >
             <div className="flex flex-row justify-between cursor-pointer">
                 <div 
                     onClick={() => {
@@ -41,73 +78,55 @@ export function RelatedOrders() {
             </div>
 
             <div className="flex flex-col flex-1 gap-8 h-full max-h-fit overflow-hidden">
-                {
-                    suggestions.length == 0 ? 
-                    <div className="flex flex-col flex-1 items-center justify-center">
-                        <p className="text-gray-400">No transactions containing this product variant</p>
-                    </div>
-                    :
-                    <div className="flex flex-col gap-2 text-white">
-                        {
-                            suggestions?.sort((a,b) => new Date(a.order_date).getTime() - new Date(b.order_date).getTime())?.map(b => {
+                {suggestions.length == 0 ?
+                <div className="flex flex-col flex-1 items-center justify-center">
+                    <p className="text-gray-400">No transactions containing this product variant</p>
+                </div>
+                :
+                <div className="flex flex-col gap-2 text-white">
+                    {sortedSuggestions.map(b =>
+                        <div key={b.id} className="bg-gray-700 px-4 py-4 pt-2 rounded-md gap-2 flex flex-col">
+                            <div className="flex flex-row items-center gap-4">
+                                <p className="font-semibold">
+                                    {moment(b.order_date).format("DD/MM/YY hh:ss")}
+                                </p>
+                                <p className="font-semibold">${b.order_total}</p>
+                                <p onClick={() => {
+                                    setKioskPanel("inv-transaction")
+                                    setInspectingTransaction({ item: b, identifier: b.products[0].id });
+                                }} className="bg-gray-600 px-2 rounded-md cursor-pointer">View Details</p>
+                            </div>
+
+                            {b.products.map(k => {
                                 return (
-                                    <div key={b.id} className="bg-gray-700 px-4 py-4 pt-2 rounded-md gap-2 flex flex-col">
-                                        <div className="flex flex-row items-center gap-4">
-                                            <p className="font-semibold">{moment(b.order_date).format("DD/MM/YY hh:ss")}</p>
-                                            <p className="font-semibold">${b.order_total}</p>
-                                            <p onClick={() => {
-                                                setKioskPanel("inv-transaction")
-                                                setInspectingTransaction({ item: b, identifier: b.products[0].id });
-                                            }} className="bg-gray-600 px-2 rounded-md cursor-pointer">View Details</p>
+                                    <div key={k.id} className="bg-gray-900 px-4 py-2 rounded-md">
+                                        <div className="flex flex-row items-center gap-4 ">
+                                            <p className="font-bold">{k.reference}</p>
+                                            <p className="bg-gray-800 text-white text-semibold px-2 rounded-full">
+                                                {k.order_type}
+                                            </p>
+
+                                            <p className="text-gray-400">{orderType(k, b)}</p>
                                         </div>
-                                        
-                                        {b.products.map(k => {
-                                            return (
-                                                <div key={k.id} className="bg-gray-900 px-4 py-2 rounded-md">
-                                                    <div className="flex flex-row items-center gap-4 ">
-                                                        <p className="font-bold">{k.reference}</p>
-                                                        <p className="bg-gray-800 text-white text-semibold px-2 rounded-full">{k.order_type}</p>
 
-                                                        <p className="text-gray-400">
-                                                            {(() => {
-                                                                switch(k.order_type) {
-                                                                    case "direct":
-                                                                        return `${k.origin.contact.name} (${k.origin.store_code})`
-                                                                    case "pickup":
-                                                                        return `${k.origin.contact.name} (${k.origin.store_code})`
-                                                                    case "quote":
-                                                                        return `By ${b.salesperson} at ${k.origin.contact.name} (${k.origin.store_code})`
-                                                                    case "shipment":
-                                                                        return `${k.origin.contact.name} (${k.origin.store_code}) -> ${k.destination?.store_code !== "000" ? k.destination?.store_code : k.destination?.contact.address.street} ${k.destination?.store_code !== "000" ? k.destination?.contact.name : k.destination?.contact.address.street2}`
-                                                                    default:
-                                                                        return ""
-                                                                }
-                                                            })()}
-                                                        </p>
-                                                    </div>
+                                        <div>
+                                            {k.products.map(n =>
+                                                <div key={n.id} className="flex flex-row items-center gap-2">
+                                                    <p className="text-gray-400">
+                                                        {n.quantity}x
+                                                    </p>
 
-                                                    <div>
-                                                        {
-                                                            k.products.map(n => {
-                                                                return (
-                                                                    <div key={n.id} className="flex flex-row items-center gap-2">
-                                                                        <p className="text-gray-400">{n.quantity}x</p>
-                                                                        <p>{n.product_name}</p>
-
-                                                                        <p>${n.product_cost}</p>
-                                                                    </div>
-                                                                )
-                                                            })
-                                                        }
-                                                    </div>
+                                                    <p>{n.product_name}</p>
+                                                    <p>${n.product_cost}</p>
                                                 </div>
-                                            )
-                                        })}
+                                            )}
+                                        </div>
                                     </div>
                                 )
-                            })
-                        }
-                    </div>
+                            })}
+                        </div>
+                    )}
+                </div>
                 }
             </div>
         </div>
