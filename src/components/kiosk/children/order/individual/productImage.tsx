6d723ae8edc90f0031cab13efdf8d1e827f9c7d9
Ben White <contact@bennjii.dev>
2023-12-29 08:54:22 +1300

    mod: refactor productImage

--- src/components/kiosk/children/order/individual/productImage.tsx
+++ src/components/kiosk/children/order/individual/productImage.tsx
@@ -1,6 +1,7 @@
 import { ContextualOrder, ContextualProductPurchase } from "@/src/utils/stockTypes";
 import Image from "next/image";
 import {Stock} from "@/generated/stock/Api";
+import {useMemo} from "react";
 
 interface ProductImageProps {
     currentOrder: ContextualOrder,
@@ -9,26 +10,38 @@ interface ProductImageProps {
 }
 
 export function ProductImage({ currentOrder, quantityHere, product }: ProductImageProps) {
+    const productQuantityStyle = useMemo(() => {
+        // If it's a direct order (must be in-stock), and has a quantity above that which is sellable
+        // by the current store - warn the operator using a red colouring.
+        if (
+            currentOrder.order_type === "direct"
+            &&
+            ((currentOrder.products.reduce((t, i) =>
+                i.variant_information.barcode == product.variant_information.barcode ? i.quantity : 0,
+                0)
+                ?? 1
+            ) > (quantityHere?.quantity.quantity_sellable ?? 0) && product.transaction_type == "Out")
+        ) return "bg-red-500"
+
+        return "bg-gray-600"
+    }, [currentOrder, product, quantityHere])
+
     return (
         <div className="relative">
-            <Image height={60} width={60} quality={100} alt="" className="rounded-sm" src={product.variant_information.images[0]}></Image>
+            <Image
+                height={60} width={60}
+                quality={100} alt=""
+                className="rounded-sm"
+                src={product.variant_information.images[0]}
+            />
 
-            {
-                currentOrder.order_type == "direct" ?
-                    (currentOrder.products.reduce((t, i) => t += (i.variant_information.barcode == product.variant_information.barcode ? i.quantity : 0), 0) ?? 1) 
-                    >
-                    (quantityHere?.quantity.quantity_sellable ?? 0) && product.transaction_type == "Out"
-                    ?
-                    <div className="bg-red-500 rounded-full flex items-center justify-center h-[30px] w-[minmax(30px, 100%)] px-1 min-h-[30px] min-w-[30px] absolute -top-3 -right-3 border-gray-900 border-4">{product.quantity}</div>
-                    :
-                    // Determine the accurate representation of a non-diminishing item.
-                    product.variant_information.stock_information.non_diminishing ?
-                    <div className="bg-gray-600 rounded-full flex items-center justify-center h-[30px] w-[minmax(30px, 100%)] px-1 min-h-[30px] min-w-[30px] absolute -top-3 -right-3 border-gray-900 border-4">{product.quantity}</div>
-                    :
-                    <div className="bg-gray-600 rounded-full flex items-center justify-center h-[30px] w-[minmax(30px, 100%)] px-1 min-h-[30px] min-w-[30px] absolute -top-3 -right-3 border-gray-900 border-4">{product.quantity}</div>
-                :
-                    <div className="bg-gray-600 rounded-full flex items-center justify-center h-[30px] w-[minmax(30px, 100%)] px-1 min-h-[30px] min-w-[30px] absolute -top-3 -right-3 border-gray-900 border-4">{product.quantity}</div>
-            }
+            <div className={
+                `${productQuantityStyle} rounded-full flex items-center justify-center h-[30px] 
+                w-[minmax(30px, 100%)] px-1 min-h-[30px] min-w-[30px] absolute -top-3 -right-3 
+                border-gray-900 border-4`}
+            >
+                {product.quantity}
+            </div>
         </div>
     )
 }
